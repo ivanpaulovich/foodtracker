2c2ed1453f36bbb4593a30e254f0b049b61f7fce
ivanpaulovich <ivan@paulovich.net>
2018-11-04 21:03:17 +0100

    Initial files

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Ivan Paulovich on 2018-11-04.
+//  Copyright © 2018 Ivan Paulovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
